0d5670828329263af72558a2cb764b9cf4be94be
Tyler Windred <tyler.windred@gmail.com>
2021-10-11 13:17:50 +0100

    Implemented random number generator

---
+++ script.js
@@ -0,0 +1,4 @@
+//random number generator taking an input number and multiplying it by a random number between 1 and 10.
+const randomNumGen = (num) => {
+    return Math.floor(Math.random() * num)
+};
